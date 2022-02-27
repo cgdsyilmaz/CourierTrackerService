c60bb5fb5cfdc00b2a393ebae8dc913f7d0d5bb1
Cagdas Han Yilmaz <cagdasyilmaz96@gmail.com>
2022-02-27 16:48:27 +0300

    Project initialized using springinitialzr

---
+++ src/main/java/com/cagdasyilmaz/couriertrackerservice/CourierTrackerServiceApplication.java
@@ -0,0 +1,13 @@
+package com.cagdasyilmaz.couriertrackerservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourierTrackerServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CourierTrackerServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cagdasyilmaz/couriertrackerservice/CourierTrackerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cagdasyilmaz.couriertrackerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourierTrackerServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
